31d28ba70d2073b6c172a2ff8110fd0609d1cb01
Sergio Jiménez <anubarakdewaarimasen@gmail.com>
2019-08-26 14:37:00 -0600

    añadida la escena de un npc provicional, reparado el bug de giro infinito del jugador

--- proyectoTanks/scripts/jugador.gd
+++ proyectoTanks/scripts/jugador.gd
@@ -6,20 +6,19 @@ export (float) var rotationSpeed = 1.5
 var velocity = Vector2()
 var rotationDir = 0
 
-
 func get_input():
+	rotationDir = 0
 	velocity = Vector2()
 	if(Input.is_action_pressed("ui_right")):
 		rotationDir += 1
 	if(Input.is_action_pressed("ui_left")):
 		rotationDir -= 1
 	if(Input.is_action_pressed("ui_down")):
-		velocity = Vector2(0, speed).rotated(rotation)
-	if(Input.is_action_pressed("ui_up")):
 		velocity = Vector2(0, -speed).rotated(rotation)
+	if(Input.is_action_pressed("ui_up")):
+		velocity = Vector2(0, speed).rotated(rotation)
 
 func _physics_process(delta):
 	get_input()
-	rotation = rotationDir * rotationSpeed * delta
+	rotation += rotationDir * rotationSpeed * delta
 	velocity = move_and_slide(velocity)
-	
