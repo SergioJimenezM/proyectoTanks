225801f02870e423909d8b58b26644745c9d44cd
Sergio Jiménez <anubarakdewaarimasen@gmail.com>
2019-08-26 11:14:05 -0600

    escena de mapa con limites, movimiento del jugador, colision del jugador con los limites, añadido bug de giro infinito cuando el tanque hace cosa indeterminada con los muros

--- proyectoTanks/scripts/Mapa.gd
+++ proyectoTanks/scripts/Mapa.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 func _ready():
 	var spawn = get_node("Position2D")
-	var tank = load("res://escenas/tankeAmarillo.tscn").instance()
+	var tank = load("res://escenas/tankeJugador.tscn").instance()
 	tank.set_position(spawn.get_position())
 	get_parent().add_child(tank)
---
+++ proyectoTanks/scripts/jugador.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (float) var rotationSpeed = 1.5
+
+var velocity = Vector2()
+var rotationDir = 0
+
+
+func get_input():
+	velocity = Vector2()
+	if(Input.is_action_pressed("ui_right")):
+		rotationDir += 1
+	if(Input.is_action_pressed("ui_left")):
+		rotationDir -= 1
+	if(Input.is_action_pressed("ui_down")):
+		velocity = Vector2(0, speed).rotated(rotation)
+	if(Input.is_action_pressed("ui_up")):
+		velocity = Vector2(0, -speed).rotated(rotation)
+
+func _physics_process(delta):
+	get_input()
+	rotation = rotationDir * rotationSpeed * delta
+	velocity = move_and_slide(velocity)
+	
