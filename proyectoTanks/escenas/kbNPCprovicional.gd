c5a11b48c3f8e133550a0f13e79c526e69bb0326
Sergio Jiménez <anubarakdewaarimasen@gmail.com>
2019-08-28 12:10:22 -0600

    añadido un npc generico de prueba

---
+++ proyectoTanks/escenas/kbNPCprovicional.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (float) var rotationSpeed = 1.5
+
+var velocity = Vector2()
+var rotationDir = 0
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+
+func buscar():
+	
+	pass
+
+func atacar():
+	
+	pass
+
+func _physics_process(delta):
+	
+	pass
