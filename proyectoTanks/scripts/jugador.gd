d2bde57644d47f7d620b8b79ade788f3625ac602
Sergio Jiménez <anubarakdewaarimasen@gmail.com>
2019-09-07 21:28:34 -0600

    añadido un posible script de disparo, añadido un bug que permite salir del escenario disparando, el disparo nunca cambia de direccion y todavia colisiona con el jugador

--- proyectoTanks/scripts/jugador.gd
+++ proyectoTanks/scripts/jugador.gd
@@ -5,6 +5,7 @@ export (float) var rotationSpeed = 1.5
 
 var velocity = Vector2()
 var rotationDir = 0
+var bala = load("res://escenas/bala.tscn")
 
 func get_input():
 	rotationDir = 0
@@ -19,6 +20,14 @@ func get_input():
 		velocity = Vector2(0, -speed).rotated(rotation)
 	if(Input.is_action_pressed("ui_cancel")):
 		pass
+	if(Input.is_action_pressed("ui_shoot")):
+		shot()
+		pass
+
+func shot():
+	var nuevaBala = bala.instance()
+	nuevaBala.inicio($Position2D.global_position, $Position2D.rotation)
+	get_parent().add_child(nuevaBala)
 
 func _physics_process(delta):
 	get_input()
